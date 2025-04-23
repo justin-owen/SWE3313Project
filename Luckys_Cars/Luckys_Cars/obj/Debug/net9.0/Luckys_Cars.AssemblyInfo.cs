1428ce5fc094d6e0bb8adfa7e179522c32c70a06
wmlane805 <wmlane2003@gmail.com>
2025-04-23 12:37:33 -0400

    Commit

--- Luckys_Cars/Luckys_Cars/obj/Debug/net9.0/Luckys_Cars.AssemblyInfo.cs
+++ Luckys_Cars/Luckys_Cars/obj/Debug/net9.0/Luckys_Cars.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Luckys_Cars")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea1a8cea6874dd6c4f3ca01a20a6bf655a687e8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d89ffa0fd01630652432a72875496fca66392d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Luckys_Cars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Luckys_Cars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
